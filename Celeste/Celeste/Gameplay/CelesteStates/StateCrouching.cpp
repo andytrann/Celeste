c7acbb68ba71017c25550ec2be1521f8386ad87e
Andy Tran <tr.andy1113@gmail.com>
2018-05-31 20:51:24 -0500

    updated statecrouching

--- Celeste/Celeste/Gameplay/CelesteStates/StateCrouching.cpp
+++ Celeste/Celeste/Gameplay/CelesteStates/StateCrouching.cpp
@@ -8,7 +8,7 @@
 #include "StateDashing.h"
 
 CelesteState * StateCrouching::HandleInput(Celeste & _celeste)
-{/*
+{
 	//calculate new direction
 	glm::ivec2 newDirection(0, 0);
 	if (Keyboard::Key(GLFW_KEY_A))
@@ -40,39 +40,46 @@ CelesteState * StateCrouching::HandleInput(Celeste & _celeste)
 		_celeste.sprite = ResourceManager::GetTexture("CrouchLeft");
 		_celeste.facingDirection = -1;
 	}
-
+	PhysicsComponent& cPhys = _celeste.GetPhysicsComponent();
 	if (Keyboard::KeyDown(GLFW_KEY_N))
 	{
-		_celeste.vel.y = -_celeste.JUMP_FORCE;
+		//_celeste.vel.y = -_celeste.JUMP_FORCE;
+		cPhys.ResetVelY();
+		cPhys.Accelerate(glm::vec2(0.0f, -Celeste::JUMP_FORCE), 1.0f);
 		return new StateInAir();
 	}
 	if (Keyboard::KeyDown(GLFW_KEY_M) && _celeste.UseDash())
 	{
-		
+		cPhys.ResetVelX();
+		cPhys.ResetVelY();
 		//if holding down and left/right
 		if (newDirection.x != 0 && newDirection.y != 0)
 		{
-			_celeste.vel = (glm::vec2)newDirection * glm::sin(glm::quarter_pi<GLfloat>()) * Celeste::DASH_FORCE;
+			//_celeste.vel = (glm::vec2)newDirection * glm::sin(glm::quarter_pi<GLfloat>()) * Celeste::DASH_FORCE;
+			cPhys.Accelerate((glm::vec2)newDirection * glm::sin(glm::quarter_pi<GLfloat>()) * Celeste::DASH_FORCE, 1.0f);
 			return new StateDashing();
 		}
 		//if only holding down
 		else
 		{
-			_celeste.vel.y = (GLfloat)newDirection.y * Celeste::DASH_FORCE;
+			//_celeste.vel.y = (GLfloat)newDirection.y * Celeste::DASH_FORCE;
+			cPhys.Accelerate((glm::vec2)newDirection * Celeste::DASH_FORCE, 1.0f);
 			return new StateDashing();
 		}
 	}
-	*/
+	
 	return nullptr;
 }
 
 void StateCrouching::Update(Celeste & _celeste, GLfloat _dt)
 {
+
+	_celeste.GetPhysicsComponent().ApplyGroundFriction(glm::vec2(1.0f, 0.0f));
 }
 
 void StateCrouching::Enter(Celeste & _celeste)
-{/*
-	_celeste.vel.x = 0.0f;
+{
+	//_celeste.GetPhysicsComponent().ResetVelX();
 
 	if (_celeste.facingDirection == 1)
 	{
@@ -82,6 +89,6 @@ void StateCrouching::Enter(Celeste & _celeste)
 	{
 		_celeste.sprite = ResourceManager::GetTexture("CrouchLeft");
 	}
-	_celeste.GetLocationState() = LocationState::ON_GROUND;*/
+	_celeste.GetLocationState() = LocationState::ON_GROUND;
 }
 
